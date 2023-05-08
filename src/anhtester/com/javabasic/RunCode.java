0952b755977e438d7bed78b55f83104fc06a7869
Anh Tester <thaian.it15@gmail.com>
2023-05-08 16:36:21 +0700

    Add 2 file

--- src/anhtester/com/javabasic/RunCode.java
+++ src/anhtester/com/javabasic/RunCode.java
@@ -1,7 +1,7 @@
 package anhtester.com.javabasic;
 
-public class TestCode {
+public class RunCode {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        
     }
 }
